41795d0eb0195427993498aff9ea9f25c8671e13
한대건 <han15050424@gmail.com>
2020-08-30 14:52:32 +0900

    '불안정한 텍스트'를 만들어낼 커스텀 에디터 스크립트를 추가한다
    추가한 스크립트의 이름은 MessageEditor이고, 추가된 위치는 Assets폴더에 있다.
    이 에디터를 통해서 손쉽게 '불안정한 텍스트'를 만들어낼 수 있도록 한다!

---
+++ Assets/MessageEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MessageEditor : EditorWindow
+{
+    [MenuItem("MessageEditor/Create Unsettled")]
+    private static void Init()
+    {
+        MessageEditor window = EditorWindow.GetWindow(typeof(MessageEditor)) as MessageEditor;
+
+        window.Show();
+        window.minSize = Vector2.one * 200f;
+    }
+
+    private void OnGUI()
+    {
+        Debug.Log("Open!");
+    }
+}

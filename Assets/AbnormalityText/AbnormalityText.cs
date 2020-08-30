2d4eb88961c3febbfe90461a18d7256b6b9faaeb
한대건 <han15050424@gmail.com>
2020-08-30 14:18:23 +0900

    기형 문자의 상위 객체인 기형 텍스트를 구현하고, 씬에 적용한다
    기형 텍스트는 텍스트를 입력할 수 있으며, 입력받은 텍스트를 기반으로
    자신의 하위 오브젝트로 존재하는 기형 문자들에게 한단어씩 적용시킨다.
    
    씬의 Canvas/AbnormalityText가 추가한 '기형 텍스트'오브젝트이다.
    그리고 그 오브젝트의 하위 오브젝트로 기형 문자를 추가했다!

--- Assets/AbnormalityText/AbnormalityText.cs
+++ Assets/AbnormalityText/AbnormalityText.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AbnormalityText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField][TextArea]
+    private string mMessage;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).TryGetComponent(out Text text))  {
+                text.text = mMessage[i].ToString();
+            }
+        }
     }
 }

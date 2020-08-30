0c016263bcae286a06bd65fccff8399ac49d3a1d
한대건 <han15050424@gmail.com>
2020-08-30 14:29:03 +0900

    프로젝트의 이름을 변경한다
    아무래도 '기형'이라는 말의 어감이 좀 불쾌해서..프로젝트를 구성하는
    각 요소들의 'abnormality'에서 이름을 '불안정한'이라는 의미의 'unsettled'로 변경한다.

--- Assets/UnsettledMessage/UnsettledText.cs
+++ Assets/UnsettledMessage/UnsettledText.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AbnormalityText : MonoBehaviour
+public class UnsettledText : MonoBehaviour
 {
     [SerializeField][TextArea]
     private string mMessage;

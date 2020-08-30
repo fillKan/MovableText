b323c2c89bb6243ea50e71b17f6c9ff27762a238
한대건 <han15050424@gmail.com>
2020-08-30 13:54:09 +0900

    첫 커밋
    라이브러리 오브 루이나의 환상체에게 사용되는 문자 연출을 구현하고,
    그것을 사용하기 쉬운 형태로 만들기 위한 프로젝트 입니다 앓~~~~
    
    현재 글자가 떨리는 기능은 구현한 상태 입니다.

---
+++ Assets/AbnormalityText/AbnormalityChar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbnormalityChar : MonoBehaviour
+{
+    [SerializeField] private  uint mWaitFrame;
+    [SerializeField] private float mVibrato;
+
+    private readonly Vector3 NormalRotation = new Vector3(0f, 0f, 1f);
+
+    private IEnumerator mEUpdate;
+
+    private void OnEnable()
+    {
+        StartCoroutine(mEUpdate = EUpdate());
+    }
+    private void OnDisable()
+    {
+        if (mEUpdate != null) {
+            StopCoroutine(mEUpdate);
+        }
+        mEUpdate = null;
+    }
+    private IEnumerator EUpdate()
+    {
+        while (gameObject.activeSelf)
+        {
+            for (uint i = 0; i < mWaitFrame; i++) { yield return null; }
+
+            transform.localRotation = Quaternion.Euler(NormalRotation * mVibrato * Random.Range(-1f, 1f));
+        }
+    }
+}

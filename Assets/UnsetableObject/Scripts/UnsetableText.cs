63e6bbe0d2369747ca7051e635209c131a810d51
한대건 <han15050424@gmail.com>
2020-09-05 16:49:06 +0900

    글자를 한글자 한글자씩 출력하는 기능을 구현한다
    이 기능은 EOutputOnebyOne코루틴을 통해 구현된다. mInterval 값의 시간을 간격으로
    글자를 한글자 한글자씩 출력한다. 출력되는 위치도 가운데를 기준으로 하여 양옆으로 퍼져나가는
    방식으로 구현했다(아직 테스트는 안해봄)아마도 앓~

--- Assets/UnsetableObject/Scripts/UnsetableText.cs
+++ Assets/UnsetableObject/Scripts/UnsetableText.cs
@@ -47,8 +47,14 @@ public class UnsetableText : MonoBehaviour
             for (float i = 0f; i < mInterval; i += Time.deltaTime * Time.timeScale) {
                 yield return null;
             }
-            // Out of one character
+            mUnsetables[iteration].gameObject.SetActive(true);
+            
+            for (int i = 0; i < iteration; i++)
+            {
+                Vector2 position = Vector2.right * mLetterSpace * iteration * 0.5f * ((iteration + 1 * 0.5f) == i ? -1f : 1f);
 
+                mUnsetables[iteration].transform.localPosition = new Vector2(mLetterSpace * iteration * 0.5f, 0);
+            }         
             iteration++;
         }        
         yield break;

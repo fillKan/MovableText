a6cf6b9ce3e6e48a83bbb5078c4b797ec20f8e41
한대건 <han15050424@gmail.com>
2020-09-06 09:57:53 +0900

    글자를 한글자씩 출력하는 기능을 쉽게 조정할 수 있게 해주는 에디터를 추가한다
    추가한 에디터의 이름은 OutputEditor이고, Tools/Unstable Text의 Output Edit를 선택하는 것으로
    활성화할 수 있다. 아직 이 외에 기능은 없다.
    
    그리고 불안정한 텍스틀를 생성하는 에디터또한 카테고리를 Tools/Unstable Text/Create로 변경하였다.

---
+++ Assets/UnsetableObject/OutputEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class OutputEditor : EditorWindow
+{
+    [MenuItem("Tools/Unstable Text/Output Edit")]
+    private static void Init()
+    {
+        OutputEditor window = EditorWindow.GetWindow(typeof(OutputEditor)) as OutputEditor;
+
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+}

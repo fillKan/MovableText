8c37b3fdad807339893992e049d938a3dd7c71d9
한대건 <han15050424@gmail.com>
2020-09-06 11:09:20 +0900

    글자를 한글자씩 출력하는 기능을 조정할 수 있는 에디터 윈도우를 추가한다
    UnsetableText를 지정하고, "Extraction"버튼을 틀릭하여 지정한 객체의 정보를 윈도우의
    필드로 불러온다. 이렇게 불러온 필드의 값을 변경한 뒤, Apply버튼을 클릭하면 필드에 변경사항이
    적용된다 와!

--- Assets/UnsetableObject/OutputEditor.cs
+++ Assets/UnsetableObject/OutputEditor.cs
@@ -4,6 +4,15 @@ using UnityEngine.UI;
 
 public class OutputEditor : EditorWindow
 {
+    private UnsetableText mUnsetable;
+
+    private string mMessage;
+
+    private float mLetterSpace;
+    private float mInterval;
+
+    private bool mIsOutputOnebyOne;
+
     [MenuItem("Tools/Unstable Text/Output Edit")]
     private static void Init()
     {
@@ -14,6 +23,52 @@ public class OutputEditor : EditorWindow
 
     private void OnGUI()
     {
+        GUILayout.Space(8f);
+        mUnsetable = (UnsetableText)EditorGUILayout.ObjectField("Edit Target", mUnsetable, typeof(UnsetableText), true);
+        GUILayout.Space(2f);
+
+        if (GUILayout.Button("Extraction"))
+        {
+            if (mUnsetable != null)
+            {
+                mMessage = mUnsetable.Message;
+
+                mLetterSpace = mUnsetable.LetterSpace;
+                mInterval    = mUnsetable.Interval;
+
+                mIsOutputOnebyOne = mUnsetable.IsOutputOnebyOne;
+            }
+            else
+            {
+                Debug.Log("You should be selection to edit target!");
+            }
+        }
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+        GUILayout.Label("Message", EditorStyles.label);
+        mMessage = EditorGUI.TextField(new Rect(2.5f, 86f, EditorGUIUtility.currentViewWidth - 7f, 18f), mMessage);
+        GUILayout.Space(21f);
+
+        GUILayout.Label("Letter Space", EditorStyles.label);
+        mLetterSpace = EditorGUILayout.FloatField(mLetterSpace);
+
+        GUILayout.Label("Interval", EditorStyles.label);
+        mInterval = EditorGUILayout.FloatField(mInterval);
+
+        mIsOutputOnebyOne = EditorGUILayout.Toggle("Print Out-OnebyOne",mIsOutputOnebyOne);
         
+        GUILayout.Space(2f);
+
+        if (GUILayout.Button("Apply") && mUnsetable != null)
+        {
+            Undo.RecordObject(mUnsetable, "Apply");
+
+            mUnsetable.Message = mMessage;
+
+            mUnsetable.LetterSpace = mLetterSpace;
+            mUnsetable.Interval    = mInterval;
+
+            mUnsetable.IsOutputOnebyOne = mIsOutputOnebyOne;
+        }
     }
 }

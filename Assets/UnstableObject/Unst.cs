57b934b898c71010a147cb97084bea0c10287082
한대건 <han15050424@gmail.com>
2020-09-09 12:26:48 +0900

    '불안정한~~'객체들을 간단하게 다룰수 있도록 해주는 클래스 Unst를 추가한다
    Unst클래스의 정적 함수를 통해서 '불안정한~~'객체를 쉽게 다룰 수 있는 기능을 제공하도록 한다.
    현재 Unst의 멤버로 존재하는 함수는 RegisterTextObject로, 기본적인 불안정텍스트 게임오브젝트를
    생성하고 간단한 설정을 진행한 불안정텍스트 객체를 반환하는 함수이다.
    
    이렇게 생성된 게임 오브젝트는 Undo에 등록된다.

---
+++ Assets/UnstableObject/Unst.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Unst
+{
+    public static UnstableText RegisterTextObject(string name, Canvas parent, Vector3 position)
+    {
+        GameObject @object = new GameObject(name, typeof(RectTransform), typeof(UnstableText));
+
+        Undo.RegisterCreatedObjectUndo(@object, name);
+
+                   @object.transform.parent        = parent.transform;
+                   @object.transform.localPosition = position;
+
+        Debug.Assert(@object.TryGetComponent(out UnstableText unstable));
+
+        return unstable;
+    }
+}

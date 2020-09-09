2733f6f0f74626e95f4d153e50d891c4dc1f3cee
한대건 <han15050424@gmail.com>
2020-09-09 15:25:36 +0900

    불안정문자간의 간격을 설정하는 확장 메서드 SetLetterSpace를 추가한다
    Transform.SetLetterSpace..와 같이 호출할 수 있으며 메시지의 총 길이, 자간, 몇번째 글자인지를
    인수로 전달받아 로컬 위치를 설정한다.

--- Assets/UnstableObject/Unst.cs
+++ Assets/UnstableObject/Unst.cs
@@ -2,6 +2,23 @@
 using UnityEngine.UI;
 using UnityEditor;
 
+public static class UnstExtension
+{
+    #region COMMENT
+    /// <summary>
+    /// 메시지의 길이, 자간, 이 객체가 몇번째 글자인지를 고려하여 (0,0)을 기준으로 자간을 설정합니다.
+    /// </summary>
+    /// <param name="messageLength">메시지의 총 길이를 지정합니다</param>
+    /// <param name="space">각 글자간의 간격을 지정합니다</param>
+    /// <param name="index">이 글자가 몇번째 글자인지를 지정합니다</param>
+    #endregion
+    public static void SetLetterSpace(this Transform transform, int messageLength, float space, int index)
+    {
+        float charOffset = (messageLength & 1).Equals(1) ? 0f : space * 0.5f;
+
+        transform.localPosition = Vector2.right * ((-messageLength / 2 + index) * space + charOffset);
+    }
+}
 public class Unst
 {
     public static UnstableText RegisterTextObject(string name, Canvas parent, Vector3 position)

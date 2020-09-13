a0b7adda05eb27f4da4267860039aad66a6ee96c
한대건 <han15050424@gmail.com>
2020-09-13 19:45:30 +0900

    TextDetailEditor에 FadeOut/In을 설정하는 영역을 추가한다
    문자 출력에 대한 설정을 하는 영역과, FadeOut/In을 설정하는 영역을 구분하고,
    Extraction버튼을 통해 FadeOut/In에 대한 값을 필드에 불러오도록 한다. Apply를 통해
    값을 설정하는건 아직..

--- Assets/UnstableObject/Editors/TextDetailEditor.cs
+++ Assets/UnstableObject/Editors/TextDetailEditor.cs
@@ -13,6 +13,13 @@ public class TextDetailEditor : EditorWindow
 
     private bool mIsPrinOnebyOne;
 
+    private FadeType mFadeType;
+
+    private float mFadeTime;
+
+    private bool mIsUsingTimeScale;
+    private bool mIsFadedDisable;
+
     [MenuItem("Tools/Unstable Text/Edit Text Detail")]
     private static void Init()
     {
@@ -36,6 +43,12 @@ public class TextDetailEditor : EditorWindow
                 mLetterSpace = mUnstable.LetterSpace;
                 mInterval    = mUnstable.Interval;
 
+                mFadeType = mUnstable.GetFadeInfo.FadeType;
+                mFadeTime = mUnstable.GetFadeInfo.FadeTime;
+
+                mIsUsingTimeScale = mUnstable.GetFadeInfo.IsUsingTimeScale;
+                  mIsFadedDisable = mUnstable.GetFadeInfo.IsFadedDisable;
+
                 mIsPrinOnebyOne = mUnstable.IsPrintOnebyOne;
             }
             else
@@ -44,9 +57,11 @@ public class TextDetailEditor : EditorWindow
             }
         }
         EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        GUILayout.Label("Print-out Option", EditorStyles.boldLabel);
+        GUILayout.Space(2.5f);
 
         GUILayout.Label("Message", EditorStyles.label);
-        mMessage = EditorGUI.TextField(new Rect(2.5f, 86f, EditorGUIUtility.currentViewWidth - 7f, 18f), mMessage);
+        mMessage = EditorGUI.TextField(new Rect(2.5f, 102f, EditorGUIUtility.currentViewWidth - 7f, 18f), mMessage);
         GUILayout.Space(21f);
 
         GUILayout.Label("Letter Space", EditorStyles.label);
@@ -55,9 +70,25 @@ public class TextDetailEditor : EditorWindow
         GUILayout.Label("Interval", EditorStyles.label);
         mInterval = EditorGUILayout.FloatField(mInterval);
 
-        mIsPrinOnebyOne = EditorGUILayout.Toggle("Print-OnebyOne",mIsPrinOnebyOne);
-        
-        GUILayout.Space(2f);
+        mIsPrinOnebyOne = EditorGUILayout.Toggle("Print-OnebyOne", mIsPrinOnebyOne);
+
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        GUILayout.Label("Fading Option", EditorStyles.boldLabel);
+        GUILayout.Space(2.5f);
+
+        GUILayout.Label("Fade Type", EditorStyles.label);
+        mFadeType = (FadeType)EditorGUILayout.EnumPopup(mFadeType);
+
+        GUILayout.Label("Fade Time", EditorStyles.label);
+        mFadeTime = EditorGUILayout.FloatField(mFadeTime);
+
+        mIsUsingTimeScale = 
+        EditorGUILayout.Toggle("Is Using Time Scale", mIsUsingTimeScale);
+
+        mIsFadedDisable =
+        EditorGUILayout.Toggle("Is Faded Disable", mIsFadedDisable);
+
+        GUILayout.Space(3f);
 
         if (GUILayout.Button("Apply") && mUnstable != null)
         {

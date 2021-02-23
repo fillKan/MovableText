6d3db8375d9182f5844adbbd07b642c8ea911443
kimbokChi <han15050424@gmail.com>
2021-02-23 10:40:24 +0900

    글자를 한 글자씩 출력하는 기능을 스크립트로 분화한다
    이 기능은 PrintOneByOne이라는 스크립트로 구현된다.
    
    Invoke함수를 실행하거나, InvokeOnAwake의 값을 '참'으로 설정하는 것으로 Awake함수가 실행될 때 실행한다.

---
+++ Assets/MovableText/Scripts/MovableComponent/PrintOneByOne.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintOneByOne : MonoBehaviour
+{
+    public bool InvokeOnAwake;
+    
+    [Header("PrintOut Property")]
+    public float IntervalTime;
+    public float LetterSpace;
+    public AnimatorUpdateMode UpdateMode;
+
+    [Header("User Property")]
+    public MovableText Owner;
+
+    private IEnumerator _PrintRoutine;
+
+    private void Awake()
+    {
+        if (InvokeOnAwake)
+        {
+            Invoke();
+        }
+    }
+    public void Invoke()
+    {
+        if (_PrintRoutine != null)
+        {
+            StopCoroutine(_PrintRoutine);
+        }
+        StartCoroutine(_PrintRoutine = PrintRoutine());
+    }
+    private IEnumerator PrintRoutine()
+    {
+        var array = Owner.GetMovableObjects();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i].gameObject.SetActive(false);
+        }
+        float DeltaTime()
+        {
+            switch (UpdateMode)
+            {
+                case AnimatorUpdateMode.Normal:
+                    {
+                        return Time.deltaTime;
+                    }
+                case AnimatorUpdateMode.AnimatePhysics:
+                    {
+                        return Time.fixedDeltaTime;
+                    }
+                case AnimatorUpdateMode.UnscaledTime:
+                    {
+                        return Time.unscaledDeltaTime;
+                    }
+                default:
+                    return Time.deltaTime;
+            }
+        }
+        for (int i = 0; i < array.Length; ++i)
+        {
+            for (float time = 0; time < IntervalTime; time += DeltaTime())
+            {
+                array[i].gameObject.SetActive(true);
+                yield return null;
+            }
+        }
+    }
+}

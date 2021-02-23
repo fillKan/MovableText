bc73823386b076f3ec65bb33fb9c3bfc01f2b0f3
kimbokChi <han15050424@gmail.com>
2021-02-23 12:00:08 +0900

    MovableText를 서서히 사라지게 하는 기능을 Gradient이라는 스크립트로 분화한다
    MovableText에 기존에 있던 '서서히 사라지게 하는 기능'을 더욱 강화하여,
    Gradient스크립트는 지정한 색상으로, 지정한 시간에 걸쳐 색상이 변하도록 하는 기능을 제공하도록 한다.
    
    이 기능은 SampleScene의 Mush오브젝트에 적용되어있다.

---
+++ Assets/MovableText/Scripts/MovableComponent/Gradient.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gradient : MonoBehaviour
+{
+    public bool InvokeOnAwake;
+    
+    [Header("Gradient Property")]
+    public float ProgressingTime;
+    public Color GradientColor = Color.white;
+    public AnimatorUpdateMode UpdateMode;
+
+    [Header("User Property")]
+    public MovableText Owner;
+
+    public float GradientPercent
+    {
+        get;
+        private set;
+    }
+    public bool IsAlreadyInit
+    {
+        get;
+        private set;
+    }
+
+    private IEnumerator _GradientRoutine;
+    private Text[] _TextArray;
+
+    private void Reset()
+    {
+        TryGetComponent(out Owner);
+    }
+    private void Awake()
+    {
+        if (InvokeOnAwake)
+        {
+            Invoke();
+        }
+    }
+    public void Invoke()
+    {
+        Initialize();
+
+        if (_GradientRoutine != null)
+        {
+            StopCoroutine(_GradientRoutine);
+        }
+        GradientPercent = 0f;
+        StartCoroutine(_GradientRoutine = GradientRoutine());
+    }
+    public void Initialize(bool forceInit = false)
+    {
+        if (!IsAlreadyInit || forceInit)
+        {
+            _TextArray = new Text[Owner.GetMovableObjects().Length];
+
+            for (int i = 0; i < _TextArray.Length; i++)
+            {
+                Owner[i].TryGetComponent(out _TextArray[i]);
+            }
+            IsAlreadyInit = true;
+        }
+    }
+    private IEnumerator GradientRoutine()
+    {
+        var textColor = new Color[_TextArray.Length];
+
+        for (int i = 0; i < _TextArray.Length; i++)
+        {
+            textColor[i] = _TextArray[i].color;
+        }
+        float DeltaTime()
+        {
+            switch (UpdateMode)
+            {
+                case AnimatorUpdateMode.Normal:
+                    {
+                        return Time.deltaTime;
+                    }
+                case AnimatorUpdateMode.AnimatePhysics:
+                    {
+                        return Time.fixedDeltaTime;
+                    }
+                case AnimatorUpdateMode.UnscaledTime:
+                    {
+                        return Time.unscaledDeltaTime;
+                    }
+                default:
+                    return Time.deltaTime;
+            }
+        }
+        for (float i = 0; GradientPercent < 1f; i += DeltaTime())
+        {
+            GradientPercent = Mathf.Clamp(i / ProgressingTime, 0f, 1f);
+
+            for (int index = 0; index < _TextArray.Length; index++)
+            {
+                _TextArray[index].color = Color.Lerp(textColor[index], GradientColor, GradientPercent);
+            }
+            yield return null;
+        }
+        _GradientRoutine = null;
+    }
+}

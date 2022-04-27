ef22fe367ce06faf471badccf37afa32dd4e22d8
kimbokChi <han15050424@gmail.com>
2022-04-27 20:30:59 +0900

    에디터 클래스의 내용을 한 번 정리한다
    코드에 보완할 점이 많다아아아!!

--- Assets/MovableText/Editor/MovableUtilityForEditor.cs
+++ Assets/MovableText/Editor/MovableUtilityForEditor.cs
@@ -4,38 +4,49 @@ using UnityEngine.UI;
 
 public static class MovableUtilityForEditor
 {
-    private const string CHAR_OBJECT_NAME_FORMAT = "Character[{0}]";
+    private const string CHAR_OBJECT_NAME_FORMAT = "Character : {0}";
+    private const string UNDO_PREFIX = "MovableUtilityForEditor : ";
 
     public static MovableText CreateMovableText(string name, Canvas canvas, Vector3 position)
     {
+        // 변경사항들을 그룹으로 저장하기 시작.
+        Undo.IncrementCurrentGroup();
+
+        string undoCreate = string.Concat(UNDO_PREFIX, $"Create MovableText ({name})");
+        string undoSetParent = string.Concat(UNDO_PREFIX, $"Set MovableText parent");
+        string undoHierarchy = string.Concat(UNDO_PREFIX, "Register to hierarchy");
+        string undoGroupName = string.Concat(UNDO_PREFIX, $"Create MovableText ({name}) completely");
+
+        // 오브젝트 생성.
         var mov = new GameObject(name, typeof(RectTransform), typeof(MovableText));
-
-        Undo.RegisterCreatedObjectUndo(mov, name);
-
-        mov.transform.parent        = canvas.transform;
+        Undo.RegisterCreatedObjectUndo(mov, undoCreate);
+        Undo.SetTransformParent(mov.transform, canvas.transform, undoSetParent);
+        
+        // 객체의 계층 상태를 실행 취소 스택에 복사.
+        Undo.RegisterFullObjectHierarchyUndo(mov, undoHierarchy);
         mov.transform.localPosition = position;
+
+        // 지금까지의 변경사항을 그룹으로 저장.
+        Undo.SetCurrentGroupName(undoGroupName);
 
         return mov.GetComponent<MovableText>();
     }
 
     public static MovableObject CreateMovableChar(int index, char letter, MovCharInfo cInfo)
     {
-        string name = string.Format(CHAR_OBJECT_NAME_FORMAT, index);
-
-        GameObject newObject = new GameObject(name, typeof(RectTransform), typeof(Text), typeof(MovableObject));
+        string name = string.Format(CHAR_OBJECT_NAME_FORMAT, index.ToString("00"));
+        var newObject = new GameObject(name, typeof(RectTransform), typeof(Text), typeof(MovableObject));
 
         Undo.RegisterCreatedObjectUndo(newObject, name);
 
         if (newObject.TryGetComponent(out Text text))
         {
-            text.fontSize  = cInfo.fontSize;          
-            text.fontStyle = cInfo.fontStyle;
-
-            text.color     = cInfo.color;
-            text.alignment = TextAnchor.MiddleCenter;
-
-            text.text = letter.ToString();
-            text.font = cInfo.font; 
+            text.fontSize   = cInfo.fontSize;          
+            text.fontStyle  = cInfo.fontStyle;
+            text.color      = cInfo.color;
+            text.alignment  = TextAnchor.MiddleCenter;
+            text.text       = letter.ToString();
+            text.font       = cInfo.font; 
         }
         if (newObject.TryGetComponent(out MovableObject movable)) 
         {
